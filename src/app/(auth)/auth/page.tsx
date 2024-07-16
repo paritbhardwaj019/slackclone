7632ff530edd3a5f86e880f226ceef72efdf6363
Parit Bhardwaj <paritbhardwaj@outlook.com>
2024-07-16 11:53:19 +0530

    addition: auth ui

--- src/app/(auth)/auth/page.tsx
+++ src/app/(auth)/auth/page.tsx
@@ -1,10 +1,34 @@
+"use client";
+
 import { Typography } from "@/components/typography";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { authSchema } from "@/lib/validations/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { BsSlack } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
+import { MdOutlineAutoAwesome } from "react-icons/md";
 import { RxGithubLogo } from "react-icons/rx";
+import { z } from "zod";
 
 export default function () {
+  const form = useForm<z.infer<typeof authSchema>>({
+    resolver: zodResolver(authSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof authSchema>) => {};
+
   return (
     <main className="min-h-screen p-5 grid text-center place-content-center bg-white">
       <div className="max-w-[450px]">
@@ -18,7 +42,10 @@ export default function () {
         </Typography>
 
         <Typography variant="p" className="opacity-90 mb-7">
-          We suggest using the email address that you use at work
+          We suggest using the{" "}
+          <span className="font-semibold">
+            email address that you use at work
+          </span>
         </Typography>
 
         <div className="flex flex-col space-y-4">
@@ -42,6 +69,43 @@ export default function () {
             <Typography variant="p">OR</Typography>
             <div className="ml-[10px] flex-1 border-t bg-neutral-300" />
           </div>
+
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <fieldset>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input placeholder="name@work-email.com" {...field} />
+                      </FormControl>
+                      <FormMessage className="text-left" />
+                    </FormItem>
+                  )}
+                />
+
+                <Button
+                  variant="secondary"
+                  type="submit"
+                  className="bg-primary-dark hover:bg-primary-dark/90 w-full my-5 text-white"
+                >
+                  <Typography variant="p">Sign in with email</Typography>
+                </Button>
+
+                <div className="px-5 py-4 bg-gray-100 rounded-sm">
+                  <div className="text-gray-500 flex items-center space-x-3">
+                    <MdOutlineAutoAwesome />
+                    <Typography variant="p">
+                      We&rsquo;ll email you a magic code for a password-free
+                      sign-in
+                    </Typography>
+                  </div>
+                </div>
+              </fieldset>
+            </form>
+          </Form>
         </div>
       </div>
     </main>
